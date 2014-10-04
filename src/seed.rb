8e283eb97473b3d4cccd406c8dcaf0d97dab490b
Dave Doolin <david.doolin@gmail.com>
2014-10-04 07:01:24 -0700

    associations in seed file

--- src/seed.rb
+++ src/seed.rb
@@ -1,2 +1,5 @@
-Animal.create(kind: 'cow', name: 'Bessie')
-Farm.create(name: "Funny")
+bessie = Animal.create(kind: 'cow', name: 'Bessie')
+Animal.create(kind: 'goat', name: 'Shredder')
+Animal.create(kind: 'dog', name: 'Rover')
+Animal.create(kind: 'cat', name: 'Wheezie')
+Farm.create(name: "Funny", animals: [bessie])
